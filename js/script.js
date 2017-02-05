cab45942b3bfe58f5c6eb63fcf83e3ec0f43c702
Alex Khalaimov <alex0880@rambler.ru>
2017-02-05 04:12:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-$(function(){
+$(function () {
+    $(".dropdown").mouseover( function () {
+        $(this).children(".sub_menu").stop().slideDown(400);
+    });
 
-    $('input').ezMark();
+    $(".dropdown").mouseleave(function(){
+        $(this).children(".sub_menu").stop().slideUp(400);
+    });
+
+    $(".jcarousel").jcarousel({
+        list: ".jcarousel-list",
+        wrap: 'circular'
+    }
+
+);
+    $(".jcarousel-prev").jcarouselControl({
+        target: '-=1'
+    });
+
+    $(".jcarousel-next").jcarouselControl({
+        target: '+=1'
+    });
+
+    $('select').styler(
+
+    );
+    $('.js-check input').ezMark();
 });
